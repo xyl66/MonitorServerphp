f85e2528dafba1c5c218e6b29884dcc2b135d69d
xyl <492113976@qq.com>
2017-03-29 09:29:01 +0800

    bugrepaire

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -74,7 +74,8 @@ class Login extends \think\Controller
             $User=new User();
             $Auth_group_access=new AuthGroupAccess();
             $aid=is_admin();
-            $isad=$User->where(array('admin_id'=>$aid))->find();
+            $Admin=Db::name('Admin');
+            $isad=$Admin->where(array('admin_id'=>$aid))->find();
             if($isad==Null||$isad['account']!='admin'){
                 return array('status'=>'error','msg'=>'您没有权限！');
             }
